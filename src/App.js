bf01a5c313f594e20c044d10a5bbec39de9a59e2
Kenua <jgvezon@gmail.com>
2022-07-05 11:55:36 -0500

    Add all robot images to print in Cards component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,73 @@
 import React from "react";
+import { nanoid } from "nanoid";
+
 import Header from "./components/Header.js";
 import Cards from "./components/Cards.js";
 
+// import images
+import robotImage1 from "./images/BombMan.png";
+import robotImage2 from "./images/BubbleMan.png";
+import robotImage3 from "./images/ClashMan.png";
+import robotImage4 from "./images/CutMan.png";
+import robotImage5 from "./images/ElecMan.png";
+import robotImage6 from "./images/FireMan.png";
+import robotImage7 from "./images/HeatMan.png";
+import robotImage8 from "./images/IceMan.png";
+import robotImage9 from "./images/MetalMan.png";
+import robotImage10 from "./images/QuickMan.png";
+import robotImage11 from "./images/SparkMan.png";
+import robotImage12 from "./images/TopMan.png";
+
+let robotsData = [
+   robotImage1,
+   robotImage2,
+   robotImage3,
+   robotImage4,
+   robotImage5,
+   robotImage6,
+   robotImage7,
+   robotImage8,
+   robotImage9,
+   robotImage10,
+   robotImage11,
+   robotImage12,
+];
+
+robotsData = robotsData.map((item) => {
+   return {
+      id: nanoid(),
+      name: item.match(/(?<=\/)[\w]*(?=\.)/gi)[0],
+      url: item,
+      randomNumber: Math.floor(Math.random() * 12) + 1,
+      clicked: false,
+   };
+});
+
 function App() {
+   const [robots, setRobots] = React.useState(robotsData);
+
+   const clickRobot = (id) => {
+      setRobots((prevState) => {
+         let newState = prevState.map((item) =>
+            item.id === id ? { ...item, clicked: !item.clicked } : item
+         );
+         return newState;
+      });
+   };
+
+   const changeRandomNumber = () => {
+      setRobots((prevState) =>
+         prevState.map((item) => ({
+            ...item,
+            randomNumber: Math.floor(Math.random() * 12) + 1,
+         }))
+      );
+   };
+
    return (
       <main>
          <Header />
-         <Cards />
+         <Cards robots={robots} />
       </main>
    );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,122 +1,27 @@
 import React from "react";
-import image1 from "../images/MM-BombMan.webp";
-import image2 from "../images/MM-IceMan.webp";
-import image3 from "../images/hombrerapido.webp";
+function Cards(props) {
+   const cardList = [...props.robots]
+      .sort((a, b) => {
+         return a.randomNumber - b.randomNumber;
+      })
+      .map((robot) => {
+         let style = {
+            backgroundImage: `url(${robot.url})`,
+         };
 
-function Cards() {
-   let st1 = {
-      backgroundImage: `url("${image1}")`,
-   };
-   let st2 = {
-      backgroundImage: `url("${image2}")`,
-   };
-   let st3 = {
-      backgroundImage: `url("${image3}")`,
-   };
+         return (
+            <div className="cards__card" style={style} key={robot.id}>
+               <div className="card__title-container card__title-container--reverse">
+                  <p>{robot.name}</p>
+               </div>
+               <div className="card__title-container card__title-container--normal">
+                  <p>{robot.name}</p>
+               </div>
+            </div>
+         );
+      });
 
-   return (
-      <section className="cards">
-         <div className="cards__card" style={st1}>
-            <div className="card__title-container card__title-container--reverse">
-               <p>Cutman</p>
-            </div>
-            <div className="card__title-container card__title-container--normal">
-               <p>Cutman</p>
-            </div>
-         </div>
-         <div className="cards__card" style={st2}>
-            <div className="card__title-container card__title-container--reverse">
-               <p>Iceman</p>
-            </div>
-            <div className="card__title-container card__title-container--normal">
-               <p>Iceman</p>
-            </div>
-         </div>
-         <div className="cards__card" style={st3}>
-            <div className="card__title-container card__title-container--reverse">
-               <p>QuickMan</p>
-            </div>
-            <div className="card__title-container card__title-container--normal">
-               <p>QuickMan</p>
-            </div>
-         </div>
-
-         <div className="cards__card" style={st1}>
-            <div className="card__title-container card__title-container--reverse">
-               <p>Cutman</p>
-            </div>
-            <div className="card__title-container card__title-container--normal">
-               <p>Cutman</p>
-            </div>
-         </div>
-         <div className="cards__card" style={st2}>
-            <div className="card__title-container card__title-container--reverse">
-               <p>Cutman</p>
-            </div>
-            <div className="card__title-container card__title-container--normal">
-               <p>Cutman</p>
-            </div>
-         </div>
-         <div className="cards__card" style={st3}>
-            <div className="card__title-container card__title-container--reverse">
-               <p>Cutman</p>
-            </div>
-            <div className="card__title-container card__title-container--normal">
-               <p>Cutman</p>
-            </div>
-         </div>
-
-         <div className="cards__card" style={st1}>
-            <div className="card__title-container card__title-container--reverse">
-               <p>Cutman</p>
-            </div>
-            <div className="card__title-container card__title-container--normal">
-               <p>Cutman</p>
-            </div>
-         </div>
-         <div className="cards__card" style={st2}>
-            <div className="card__title-container card__title-container--reverse">
-               <p>Cutman</p>
-            </div>
-            <div className="card__title-container card__title-container--normal">
-               <p>Cutman</p>
-            </div>
-         </div>
-         <div className="cards__card" style={st3}>
-            <div className="card__title-container card__title-container--reverse">
-               <p>Cutman</p>
-            </div>
-            <div className="card__title-container card__title-container--normal">
-               <p>Cutman</p>
-            </div>
-         </div>
-
-         <div className="cards__card" style={st1}>
-            <div className="card__title-container card__title-container--reverse">
-               <p>Cutman</p>
-            </div>
-            <div className="card__title-container card__title-container--normal">
-               <p>Cutman</p>
-            </div>
-         </div>
-         <div className="cards__card" style={st2}>
-            <div className="card__title-container card__title-container--reverse">
-               <p>Cutman</p>
-            </div>
-            <div className="card__title-container card__title-container--normal">
-               <p>Cutman</p>
-            </div>
-         </div>
-         <div className="cards__card" style={st3}>
-            <div className="card__title-container card__title-container--reverse">
-               <p>Cutman</p>
-            </div>
-            <div className="card__title-container card__title-container--normal">
-               <p>Cutman</p>
-            </div>
-         </div>
-      </section>
-   );
+   return <section className="cards">{cardList}</section>;
 }
 
 export default Cards;
