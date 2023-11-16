7c9faef3ebfa7f3c61f97a1c565e2a214067fd9a
Kenua <juanestebanmedinag@gmail.com>
2023-11-16 11:52:25 -0500

    Change app's name
    Modified 'name' prop in package.json and 'base' prop in
    vite.config.js to change the app's name for Github pages.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/megaman-memory-card-game',
+  base: '/pokemon-memory-card-game',
   plugins: [react()],
 })
